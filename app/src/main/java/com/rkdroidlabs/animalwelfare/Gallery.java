78e0333584ffc97e9334d473faed5c516511d78f
rkdroidlabs <64827489+rkdroidlabs@users.noreply.github.com>
2020-06-25 11:25:57 +0530

    Firebase for gallery added

--- app/src/main/java/com/rkdroidlabs/animalwelfare/Gallery.java
+++ app/src/main/java/com/rkdroidlabs/animalwelfare/Gallery.java
@@ -1,14 +1,53 @@
 package com.rkdroidlabs.animalwelfare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Gallery extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    FirebaseDatabase mFirebaseDatabase;
+    DatabaseReference mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        mRef = mFirebaseDatabase.getReference("Data");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<Model, ViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Model, ViewHolder>(
+                        Model.class,
+                        R.layout.row,
+                        ViewHolder.class,
+                        mRef
+                ) {
+                    @Override
+                    protected void populateViewHolder(ViewHolder viewHolder, Model model, int i) {
+
+                        viewHolder.setDetails(getApplicationContext(), model.getTitle(), model.getImage());
+
+                    }
+                };
+        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
     }
 }
---
+++ app/src/main/java/com/rkdroidlabs/animalwelfare/Model.java
@@ -0,0 +1,24 @@
+package com.rkdroidlabs.animalwelfare;
+
+public class Model {
+
+    String title, image ;
+
+    public Model(){}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
